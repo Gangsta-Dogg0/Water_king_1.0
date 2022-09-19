7fb94881b098a1678612f9a0749be2d5c90e8592
Gangsta-Dogg0 <98046976+Gangsta-Dogg0@users.noreply.github.com>
2022-09-19 14:01:22 +0200

    Revert "pp"
    This reverts commit 453e6641a548210a5d8a531bdfbe0428827ec4bf.

--- Vand_ko_UWU/Assets/Scripts/PlayerControler.cs
+++ Vand_ko_UWU/Assets/Scripts/PlayerControler.cs
@@ -7,24 +7,40 @@ public class PlayerControler : MonoBehaviour
     public Rigidbody2D playerRB;
     public float movementSpeed = 69f;
     public float jumpHeigth = 40;
-
-    public Collider2D groundCheck;
+    public float jumpLength = 20;
 
     private Vector2 velocity;
 
 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
     private void FixedUpdate() {
 
         //moves the player on the x axis
         velocity.x = Input.GetAxis("Horizontal") * movementSpeed;
 
         //moves the player on the y axis
-        if (Input.GetButtonDown("Jump") && groundCheck.IsTouchingLayers(6))
-        {
-            print("hi");
+        if (Input.GetKeyDown(KeyCode.W)){
+            SetJump();
         }
 
         playerRB.AddForce(velocity * Time.deltaTime);
     }
 
+    private void SetJump()
+    {
+        for (int i = 5; i < -jumpLength; i--)
+        {
+            velocity.y = jumpLength * (i * i) * jumpHeigth;
+            print(velocity.y * Time.deltaTime);
+        }
+
+    }
 }
