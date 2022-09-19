7e8bcb0ce8e4173a9bd9897a3540b2a1fb9a6c4b
Gangsta-Dogg0 <98046976+Gangsta-Dogg0@users.noreply.github.com>
2022-09-19 11:51:38 +0200

    some of player con

--- Vand_ko_UWU/Assets/Scripts/PlayerControler.cs
+++ Vand_ko_UWU/Assets/Scripts/PlayerControler.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D playerRB;
+    public float movementSpeed = 69f;
+    public float jumpHeigth = 40;
+    public float jumpLength = 20;
+
+    private Vector2 velocity;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+    private void FixedUpdate() {
+
+        //moves the player on the x axis
+        velocity.x = Input.GetAxis("Horizontal") * movementSpeed;
+
+        //moves the player on the y axis
+        if (Input.GetKeyDown(KeyCode.W)){
+            SetJump();
+        }
+
+        playerRB.AddForce(velocity * Time.deltaTime);
+    }
+
+    private void SetJump()
+    {
+        for (int i = 5; i < -jumpLength; i--)
+        {
+            velocity.y = jumpLength * (i * i) * jumpHeigth;
+            print(velocity.y * Time.deltaTime);
+        }
+
     }
 }
