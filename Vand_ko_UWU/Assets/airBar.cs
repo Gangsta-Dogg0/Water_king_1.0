7d3c13424eebbfaef95917f9237ab01560e062cf
Gangsta-Dogg0 <98046976+Gangsta-Dogg0@users.noreply.github.com>
2022-09-20 10:08:22 +0200

    pp

--- Vand_ko_UWU/Assets/airBar.cs
+++ Vand_ko_UWU/Assets/airBar.cs
@@ -2,20 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class airBar : MonoBehaviour
 {
     public Slider airbar;
     public float air = 50f;
+    public float suficationAmount = 4.20f;
+    public Gradient gradient;
+    public Image colur;
 
+    private float curentAir;
 
     void Start()
     {
-        
+        airbar.value = air;
+        curentAir = air;
     }
 
     void Update()
+    {
+        curentAir -= suficationAmount * Time.deltaTime;
+        airbar.value = curentAir;
+
+        colur.color = gradient.Evaluate(airbar.value / 100);
+
+        if (curentAir <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
     {
         
     }
