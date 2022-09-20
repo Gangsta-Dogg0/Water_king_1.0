1bddad75df5f79046401ffe044fd2de0150df94a
Gangsta-Dogg0 <98046976+Gangsta-Dogg0@users.noreply.github.com>
2022-09-20 09:07:22 +0200

    CUM IN ASS

---
+++ Vand_ko_UWU/Assets/airBar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class airBar : MonoBehaviour
+{
+    public Slider airbar;
+    public float air = 50f;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
