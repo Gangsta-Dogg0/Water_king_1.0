9a4057da18f0d28d105f3438ce3a54d17e442f79
Gangsta-Dogg0 <98046976+Gangsta-Dogg0@users.noreply.github.com>
2022-09-21 08:38:30 +0200

    sed i bukse

--- Vand_ko_UWU/Assets/Scripts/cameraStop.cs
+++ Vand_ko_UWU/Assets/Scripts/cameraStop.cs
@@ -13,10 +13,15 @@ public class cameraStop : MonoBehaviour
     void Update()
     {
         float x = player.position.x;
+        float y = player.position.y;
 
         //why math dont work pls help???? :( <--- me rn because of it dont work
         x = Mathf.Clamp(x, -19, 19);
-        
-        transform.position = new Vector3(x,-1,-10);
+        if (Input.GetButtonDown("b"))
+        {
+            y = y + 10;
+        }
+
+        transform.position = new Vector3(x, y,-10);
     }
 }
